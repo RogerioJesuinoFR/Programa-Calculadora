2c9b9cfa3673baebe200a3755b0364f4dda644a9
Rogerio Jesuino Ferraz Rocha <rogerio.ferrazyyz@gmail.com>
2023-07-18 19:20:50 -0300

    Adicionei uma parte gráfica da Calculadora
    Fiz como uma tabela para as operações possíveis

--- Programa principal.c
+++ Programa principal.c
@@ -1,20 +1,24 @@
 /*Programa de calculadora*/
 
-#include<stdio.h>
+#include <stdio.h>
 
-int main()
+void MOSTRAR_CALCULADORA();
+
+int main() 
 {
     float NUM1, NUM2, RES = 0;
     char OPERACAO;
-    printf("\n");
-    scanf("%f", &NUM1);
     
-    do
+    printf("\n\t");
+    scanf("%f", &NUM1);
+
+    do 
     {
-        printf("\n Digite a operacao (Digite S para sair) : ");
+        MOSTRAR_CALCULADORA();
         scanf(" %c", &OPERACAO);
-        printf("\n");
-        if (OPERACAO == 's' || OPERACAO == 'S')
+        printf("\n\t");
+        
+        if (OPERACAO == 's' || OPERACAO == 'S') 
         {
             printf("\nSaindo do programa...\n\n");
             break;
@@ -22,7 +26,7 @@ int main()
         
         scanf("%f", &NUM2);
         
-        switch (OPERACAO)
+        switch (OPERACAO) 
         {
             case '+':
                 RES = NUM1 + NUM2;
@@ -34,18 +38,34 @@ int main()
                 RES = NUM1 * NUM2;
                 break;
             case '/':
-                RES = NUM1 / NUM2;
+                if (NUM2 != 0) // Verifica divisão por zero
+                    RES = NUM1 / NUM2;
+                else 
+                {
+                    printf("Erro: Divisao por zero!\n");
+                    continue; // Retorna ao início do loop sem alterar NUM1 e NUM2
+                }
+                break;
+            case '=':
+                printf("\n%.2f\n", RES);
                 break;
             default:
                 printf("Opcao invalida!\n");
-                break;
+                continue; // Retorna ao início do loop sem alterar NUM1 e NUM2
         }
         
-        printf("\n%.2f\n", RES);
         NUM1 = RES;
         NUM2 = 0;
         
     } while (OPERACAO != 's' && OPERACAO != 'S');
     
     return 0;
+}
+
+void MOSTRAR_CALCULADORA()
+{
+    printf("  Digite a operacao:\n");
+        printf(" \xDA\xC4\xC4\xC4\xC2\xC4\xC4\xC4\xC2\xC4\xC4\xC4\xC2\xC4\xC4\xC4\xC2\xC4\xC4\xC4\xC2\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xBF\n");
+        printf(" \xB3 + \xB3 - \xB3 * \xB3 / \xB3 = \xB3 (Digite S para sair) \xB3\n");
+        printf(" \xC0\xC4\xC4\xC4\xC1\xC4\xC4\xC4\xC1\xC4\xC4\xC4\xC1\xC4\xC4\xC4\xC1\xC4\xC4\xC4\xC1\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xD9\n\t");
 }
