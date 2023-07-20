ddc489d78ab34b8514d72a40cc0b5f64d4880a39
Rogerio Jesuino Ferraz Rocha <rogerio.ferrazyyz@gmail.com>
2023-07-20 20:25:31 -0300

    Programa Básico Completo
    Adicionei a biblioteca <windowns.h> para poder direcionar o cursor para o usuário digitar dentro do campo destinado

--- Programa principal.c
+++ Programa principal.c
@@ -1,71 +1,95 @@
 /*Programa de calculadora*/
 
 #include <stdio.h>
+#include <windows.h> // biblioteca windows.h para usar SetConsoleCursorPosition()
 
-void MOSTRAR_CALCULADORA();
+#define DISPLAY "\n\xDA\xC4\xC4\xC4\xC2\xC4\xC4\xC4\xC2\xC4\xC4\xC4\xC2\xC4\xC4\xC4\xBF\n\xB3 7 \xB3 8 \xB3 9 \xB3 / \xB3\n\xC3\xC4\xC4\xC4\xC5\xC4\xC4\xC4\xC5\xC4\xC4\xC4\xC5\xC4\xC4\xC4\xB4\n\xB3 4 \xB3 5 \xB3 6 \xB3 * \xB3\n\xC3\xC4\xC4\xC4\xC5\xC4\xC4\xC4\xC5\xC4\xC4\xC4\xC5\xC4\xC4\xC4\xB4\n\xB3 1 \xB3 2 \xB3 3 \xB3 - \xB3\n\xC3\xC4\xC4\xC4\xC5\xC4\xC4\xC4\xC5\xC4\xC4\xC4\xC5\xC4\xC4\xC4\xB4\n\xB3 C \xB3 0 \xB3 . \xB3 + \xB3 (pressione S para sair)\n\xC3\xC4\xC4\xC4\xC1\xC4\xC4\xC4\xC1\xC4\xC4\xC4\xC1\xC4\xC4\xC4\xB4\n\xB3               \xB3\n\xC0\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xD9"
+
+void MOSTRAR_DISPLAY();
 
 int main() 
 {
-    float NUM1, NUM2, RES = 0;
+    float NUM1, NUM2;
     char OPERACAO;
-    
-    printf("\n\t");
-    scanf("%f", &NUM1);
 
     do 
     {
-        MOSTRAR_CALCULADORA();
-        scanf(" %c", &OPERACAO);
-        printf("\n\t");
-        
-        if (OPERACAO == 's' || OPERACAO == 'S') 
+        system("cls"); // Limpa a tela antes de exibir o display
+
+        printf(DISPLAY);
+
+        COORD coord; // Estrutura para definir as coordenadas do cursor
+        coord.X = 2; // Posição X da coordenada (coluna)
+        coord.Y = 10; // Posição Y da coordenada (linha)
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+
+        scanf("%f", &NUM1);
+
+        do 
         {
-            printf("\nSaindo do programa...\n\n");
-            break;
-        }
-        
-        scanf("%f", &NUM2);
-        
-        switch (OPERACAO) 
-        {
-            case '+':
-                RES = NUM1 + NUM2;
+            system("cls");
+
+            printf(DISPLAY);
+
+            COORD coord;
+            coord.X = 2;
+            coord.Y = 10;
+            SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+
+            // Limpar o espaço antes de imprimir o número
+            printf("%.2f\a", NUM1);
+
+            scanf(" %c", &OPERACAO);
+
+            if (OPERACAO == 's' || OPERACAO == 'S') 
+            {
+                printf("\n\nSaindo do programa...\n\n");
                 break;
-            case '-':
-                RES = NUM1 - NUM2;
+            } else if (OPERACAO == 'c' || OPERACAO == 'C')
+            {
+                NUM1 = 0;
                 break;
-            case '*':
-                RES = NUM1 * NUM2;
-                break;
-            case '/':
-                if (NUM2 != 0) // Verifica divisão por zero
-                    RES = NUM1 / NUM2;
-                else 
-                {
-                    printf("Erro: Divisao por zero!\n");
+            }
+
+            scanf("%f", &NUM2);
+
+            system("cls");
+
+            switch (OPERACAO) 
+            {
+                case '+':
+                    NUM1 += NUM2;
+                    break;
+                case '-':
+                    NUM1 -= NUM2;
+                    break;
+                case '*':
+                    NUM1 *= NUM2;
+                    break;
+                case '/':
+                    if (NUM2 != 0) // Verifica divisão por zero
+                        NUM1 /= NUM2;
+                    else 
+                    {
+                        printf("Erro: Divisao por zero!\n");
+                        continue; // Retorna ao início do loop sem alterar NUM1 e NUM2
+                    }
+                    break;
+                default:
+                    printf("Opcao invalida!\n");
                     continue; // Retorna ao início do loop sem alterar NUM1 e NUM2
-                }
-                break;
-            case '=':
-                printf("\n%.2f\n", RES);
-                break;
-            default:
-                printf("Opcao invalida!\n");
-                continue; // Retorna ao início do loop sem alterar NUM1 e NUM2
-        }
-        
-        NUM1 = RES;
-        NUM2 = 0;
-        
-    } while (OPERACAO != 's' && OPERACAO != 'S');
-    
+            }
+
+        } while (OPERACAO != 's' && OPERACAO != 'S');
+
+    } while (OPERACAO == 'c' || OPERACAO == 'C');
+
+    getch();
     return 0;
 }
 
-void MOSTRAR_CALCULADORA()
-{
-    printf("  Digite a operacao:\n");
-        printf(" \xDA\xC4\xC4\xC4\xC2\xC4\xC4\xC4\xC2\xC4\xC4\xC4\xC2\xC4\xC4\xC4\xC2\xC4\xC4\xC4\xC2\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xBF\n");
-        printf(" \xB3 + \xB3 - \xB3 * \xB3 / \xB3 = \xB3 (Digite S para sair) \xB3\n");
-        printf(" \xC0\xC4\xC4\xC4\xC1\xC4\xC4\xC4\xC1\xC4\xC4\xC4\xC1\xC4\xC4\xC4\xC1\xC4\xC4\xC4\xC1\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xC4\xD9\n\t");
-}
+
+
+
+
+
